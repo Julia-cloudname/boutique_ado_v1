7b6eeab67e04aef5d2c83fcb6c1e1e15beb371bb
Julia <malukjulia2014@gmail.com>
2023-10-08 10:09:41 +0000

    Deployment test S3 bucket 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,14 +180,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-
+    
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-student'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-student-2'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -202,6 +197,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
