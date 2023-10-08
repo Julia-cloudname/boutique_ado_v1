df50afc74edd694cee70e0750d3f217eb0f0a474
Julia <malukjulia2014@gmail.com>
2023-10-08 17:39:18 +0000

    062 - base.css and login/reg refactor

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [ 'boutique-ado-student.herokuapp.com', 'localhost', 'boutique-ado-student-9eba64bd7f42.herokuapp.com']
+ALLOWED_HOSTS = [ 'boutique-ado-student.herokuapp.com', '8000-juliacloudn-boutiqueado-tz65jil1r1a.ws-eu105.gitpod.io', 'boutique-ado-student-9eba64bd7f42.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://8000-juliacloudn-boutiqueado-tz65jil1r1a.ws-eu104.gitpod.io', 'https://8000-juliacloudn-boutiqueado-tz65jil1r1a.ws-eu105.gitpod.io']
 
 # Application definition
